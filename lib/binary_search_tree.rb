3c6f9ee56fff5b68f5d4c8b9580e63b16da8d311
Steven McVey <smcvey1210@gmail.com>
2023-03-05 23:42:09 -0800

    commit tree ab91ad93507d3d3a4144d39bd1189c644fcbf03a

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,4 +1,5 @@
 require_relative './node'
+require 'pry'
 
 class BinarySearchTree
   attr_accessor :root
@@ -8,11 +9,58 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+    if root.nil?
+      return nil
+    end
+    node = root
+    response = nil
+    while node.value != value
+      node = value > node.value ? node.right : node.left
+
+      if node.nil?
+        return nil
+      end
+
+      if node.value == value
+        response = node
+      end
+    end
+    response
   end
 
   def insert(value)
-    # your code here
+    if search(value)
+      return nil
+    end
+
+    node = Node.new(value)
+
+    if root.nil?
+      @root = node
+      return node
+    end
+
+    check = @root
+    check_next = value > root.value ? root.right : root.left
+
+    while check_next != nil
+      check = check_next
+      check_next = helper(check_next, value)
+    end
+
+    if value > check.value
+      return check.right
+    else
+      return check.left
+    end
+  end
+
+  def helper(check, value)
+    if value > check.value
+      return check.right
+    else
+      return check.left
+    end
   end
 
 end 
